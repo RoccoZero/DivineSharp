b1048e920f42c8d28c097a4d155be461b0a4bb46
RoccoZero <osmanbonus007@hotmail.com>
2021-02-16 19:03:19 +0300

    update

---
+++ WolfInfo/Context.cs
@@ -0,0 +1,21 @@
+﻿using Divine.Menu;
+using Divine.Menu.Items;
+
+namespace WolfInfo
+{
+    internal sealed class Context
+    {
+        public readonly RootMenu RootMenu;
+        private readonly CreepHPBars CreepHPBars;
+
+        public Context()
+        {
+            RootMenu = MenuManager.CreateRootMenu("Ender_Wolf");
+            CreepHPBars = new CreepHPBars(this);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
