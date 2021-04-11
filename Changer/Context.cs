b82a321d93081fd4a00afa428b14d88c65d3f8c3
vana41203 <37879239+vana41203@users.noreply.github.com>
2021-04-12 00:59:20 +0300

    Added Changer

---
+++ Changer/Context.cs
@@ -0,0 +1,50 @@
+﻿using Divine;
+using Divine.Menu.Items;
+using System;
+using Divine.Menu.EventArgs;
+
+namespace Changer
+{
+    internal class Context
+    {
+        private readonly Menu Menu;
+
+        public Context()
+        {
+            Menu = new Menu();
+
+            //GameManager.RiverType = (RiverType)Array.IndexOf<string>(Menu.RiverTypes, Menu.RiverType.Value);
+
+            //foreach (var tree in EntityManager.GetEntities<Tree>())
+            //{
+            //    tree.SetModel(Menu.TreeModel.Value + ".vmdl");
+            //}
+
+            //Console.WriteLine();
+            Menu.RiverType.ValueChanged += RiverType_ValueChanged;
+            Menu.TreeModel.ValueChanged += TreeModel_ValueChanged;
+        }
+
+        private void TreeModel_ValueChanged(MenuSelector selector, SelectorEventArgs e)
+        {
+            if (e.NewValue != "OFF")
+            {
+                //Console.WriteLine(e.NewValue + ".vmdl");
+                foreach (var tree in EntityManager.GetEntities<Tree>())
+                {
+                    tree.SetModel(e.NewValue + ".vmdl");
+                }
+            }
+        }
+
+        private void RiverType_ValueChanged(MenuSelector selector, SelectorEventArgs e)
+        {
+            GameManager.RiverType = (RiverType)Array.IndexOf<string>(Menu.RiverTypes, e.NewValue);
+        }
+
+        internal void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ Changer/Menu.cs
@@ -0,0 +1,54 @@
+﻿using Divine;
+using Divine.Menu;
+using Divine.Menu.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changer
+{
+    internal sealed class Menu
+    {
+        public readonly RootMenu RootMenu;
+        public readonly MenuSelector RiverType;
+        public readonly MenuSelector TreeModel;
+        public readonly string[] RiverTypes = new string[]
+        {
+            "Default",
+            "Chrome",
+            "Dry",
+            "Slime",
+            "Oil",
+            "Electric",
+            "Potion",
+            "Blood"
+        };
+            
+        public readonly string[] TreeModels = new string[]
+        {
+            "OFF",
+            "models/props_stone/stone_column002a",
+            "models/props_structures/pumpkin003",
+            "models/props_gameplay/pumpkin_bucket",
+            "models/particle/snowball",
+            "models/effects/fountain_radiant_00",
+            "models/props_magic/bad_magic_tower001"
+        };
+
+        public Menu()
+        {
+            RootMenu = MenuManager.CreateRootMenu("Changer");
+            RiverType = RootMenu.CreateSelector(
+                 "River type",
+                 RiverTypes
+            );
+            TreeModel = RootMenu.CreateSelector(
+                 "Tree model",
+                 TreeModels
+            );
+
+        }
+    }
+}
